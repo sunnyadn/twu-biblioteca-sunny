2674a950b1cf2f4d0ea624bf19fef07d47701b0c
Sunny <sunnyadn@foxmail.com>
2020-08-02 03:14:46 +0800

    Close #3 Story 1.3 View author and publication year on all books

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -5,6 +5,6 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         Prompt prompt = new Prompt(System.out);
         prompt.printWelcomeMessage();
-        prompt.listAllBookTitles();
+        prompt.listAllBooksWithAuthorAndPublicationYear();
     }
 }
